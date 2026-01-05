78cd5f9e6d4fd7c5c40adac2167acb8744fa89e6
sillyburg <sillyburgerprivalt@gmail.com>
2026-01-04 21:25:10 -0300

    0.8.0 support

---
+++ scripts/modules/PAPauseSubState.hx
@@ -0,0 +1,16 @@
+import funkin.play.PauseSubState;
+
+import funkin.ui.MusicBeatSubState;
+
+class PAPauseSubState extends MusicBeatSubState
+{
+    public function new()
+    {
+        super();
+    }
+
+    public function create()
+    {
+        trace("HAIHAI");
+    }
+}
